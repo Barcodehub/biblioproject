7bf7f0156c9ebb7c6095513017898dba041eef0a
sergio-andres-d <100731961+sergio-andres-d@users.noreply.github.com>
2023-05-24 23:06:48 -0500

    modelo reserva update

---
+++ src/main/java/modelo/Reserva.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Reserva {
+    private int usuarioId;
+    private int libroId;
+    private Date fechaLimite;
+    private String Estado;
+
+    public Reserva() {
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public int getLibroId() {
+        return libroId;
+    }
+
+    public void setLibroId(int libroId) {
+        this.libroId = libroId;
+    }
+
+    public Date getFechaLimite() {
+        return fechaLimite;
+    }
+
+    public void setFechaLimite(Date fechaLimite) {
+        this.fechaLimite = fechaLimite;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+    
+}
