228bc73a8648adac1e93bbcf1ddf32f5e5014c9a
barkoHub <brayanalexanderbc@ufps.edu.co>
2023-05-27 19:56:09 -0500

    Olvidar contraseña con validadores de email y contraseña nueva ccon codigo de autentificacion

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -1,8 +1,14 @@
-
 package controlador;
 
+import Config.Conexion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,50 +16,123 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.Persona;
 import modeloDAO.PersonaDAO;
+
 //validar el login
 
 public class Controlador extends HttpServlet {
-    PersonaDAO dao=new PersonaDAO();
-    Persona p=new Persona();
-    int r;
 
+    PersonaDAO dao = new PersonaDAO();
+    Persona p = new Persona();
+    int r;
+    String editPass = "editPass.jsp";
+    String pass, pass2 = "";
+    int codigoauth = 0;
+    int contid = 0;
+
+    public static void main(String[] args) {
+        
+    }
     
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String acceso = "";
         response.setContentType("text/html;charset=UTF-8");
         String accion = request.getParameter("accion");
-        if(accion.equals("Ingresar")){
-            String nom=request.getParameter("txtnom");
-            String correo=request.getParameter("txtCorreo");
-            String pass=request.getParameter("txtPass");
+        if (accion.equals("Ingresar")) {
+            String nom = request.getParameter("txtnom");
+            String correo = request.getParameter("txtCorreo");
+            String pass = request.getParameter("txtPass");
             p.setNom(nom);
             p.setCorreo(correo);
             p.setPass(pass);
-            r=dao.validar(p);
-            if(r==1){
+            r = dao.validar(p);
+            if (r == 1) {
                 request.getSession().setAttribute("nom", nom);
                 request.getSession().setAttribute("correo", correo);
                 request.getSession().setAttribute("pass", pass);
-                request.getRequestDispatcher("Principal.jsp").forward(request, response);
-            }else{
+                request.getRequestDispatcher("Principal.jsp").forward(request, response); //me entra si valida, CRUCIAL PARA EL ROL ADMIN Y UDUARIO LUEGO
+            } else {
                 request.getRequestDispatcher("index.jsp").forward(request, response);
             }
-        }else{
-            request.getRequestDispatcher("index.jsp").forward(request, response);
-           
+
+            
+            
+            
+            
+        } else if (accion.equals("ValidarCorreo")) {
+            //si el correo existe en la BD
+            String correo = request.getParameter("txtCorreo");
+            p.setCorreo(correo);
+            r = dao.validarCorreo(p);
+            if (r == 1) {
+//                request.getSession().setAttribute("correo", correo);
+
+                pass = request.getParameter("txtPass1");
+                pass2 = request.getParameter("txtPass2");
+                System.out.println("PASS: " + pass2);
+
+                if (pass.equals(pass2)) { //si ambas passwords son iguales
+
+                    codigoauth = (int) (Math.random() * (99999)) + 1; //codigo de autentificacion aleatorio
+                    
+                    System.out.println("el numero es: " + codigoauth);
+
+                 
+       
+                    //CODIGO PARA ENVIAR CODIGODEAUTENTIFICACION AL CORREO AQUI
+                   
+                    
+                    
+                    
+                    
+                    //para extraer el id del correo del usuario
+                    PersonaDAO dao = new PersonaDAO();
+                    List<Persona> list = dao.listar();
+                    Iterator<Persona> iter = list.iterator();
+                    Persona per = null;
+
+                    while (iter.hasNext()) {
+                        per = iter.next();
+                        
+                        if (correo.equals(per.getCorreo())) {
+//                            System.out.println("id es: " + contid);
+                            contid++;
+                        }
+                    }
+
+                    System.out.println("getcorreo: " + p.getCorreo());
+                    System.out.println("el id de " + correo + " es: " + contid);
+
+                    //llevamos para que el usuario ingrese el codigo de autentificacion
+                    request.getRequestDispatcher("editPass.jsp").forward(request, response);
+
+                }
+
+//               
+            }
+
+        } else if (accion.equals("ValidarCodigo")) {
+
+            //codigo validado ahora actualizece la contraseña
+            int codigo = Integer.parseInt(request.getParameter("txtcod"));
+
+            System.out.println("codigo: " + codigo);
+
+            if (codigoauth == codigo) {
+                p.setId(contid);
+                p.setPass(pass);
+                dao.editPass(p);
+
+            }
+
+        } else {
+            //Correo ingresado incorrecto---vueleve a intenetarlo
+            request.getRequestDispatcher("validarCorreo.jsp").forward(request, response);
+
         }
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
---
+++ src/main/webapp/validarCorreo.jsp
@@ -0,0 +1,87 @@
+<%-- 
+    Document   : validarCorreo
+    Created on : 27-may-2023, 14:17:52
+    Author     : BRAYAN
+--%>
+
+<%@page import="modelo.Persona"%>
+<%@page import="modeloDAO.PersonaDAO"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.sql.Statement"%>
+<%@page import="Config.Conexion"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+         <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.0/normalize.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body>
+        
+        
+          
+          
+        <h1>Reestablecer Contraseña</h1>
+<!--        //-->
+        <form action="Controlador">
+        <div class="form-group">
+                    <label>Escribe el Correo asociado a tu Cuenta: </label>
+                    <input class="form-control" type="email" name="txtCorreo" placeholder="example@gmail.com">
+                </div>
+                
+            <div class="form-group">
+                    <label>New Password: </label>
+                    <input class="form-control" type="password" name="txtPass1" placeholder="Ingrese Contraseña">
+                </div>
+            <div class="form-group">
+                    <label>Confirma la New Password: </label>
+                    <input class="form-control" type="password" name="txtPass2" placeholder="Ingrese Contraseña">
+                </div>
+            
+            <input class="btn btn-primary bottom-right btn-block" type="submit" name="accion" value="ValidarCorreo">
+            
+            
+            
+            
+<!--            if comparar BD
+if ambas passwords iguales
+            if numeros aleatorio env al correo con el num escrito por el usuari-->
+        </form>
+        
+        
+         <%
+                Conexion con = new Conexion();
+                Statement smt;
+                ResultSet rs;
+                smt = con.getConnection().createStatement();
+                rs = smt.executeQuery("select * from libro");
+
+                //recorrer hasta el id de ese correo
+//          if(p.getId()){
+//          
+//             }
+
+//             PersonaDAO dao=new PersonaDAO();
+//              int id=Integer.parseInt((String)request.getAttribute("idper"));
+//              Persona p=(Persona)dao.list(id);
+             
+
+
+
+            %>
+        
+        
+        
+        
+        
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    </body>
+</html>
